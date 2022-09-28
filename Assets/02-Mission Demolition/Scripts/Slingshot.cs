9a7e8f818328f7786e7e7b62e7352b1e32115f55
sachi-joshi <76460499+sachi-joshi@users.noreply.github.com>
2022-09-28 13:00:28 -0400

    created assets and materials

---
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnMouseEnter()
+    {print ("Slingshot: OnMouseEnter()");
+        
+    }
+
+    // Update is called once per frame
+    void OnMouseExit()
+    {
+        print("Slingshot: OnMouseExit()");
+    }
+}
