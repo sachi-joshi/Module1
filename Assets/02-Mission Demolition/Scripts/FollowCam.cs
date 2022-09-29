cb8300e6fe7a135c7ca3d2fd870638bcdf1aa5a6
sachi-joshi <76460499+sachi-joshi@users.noreply.github.com>
2022-09-29 18:23:55 -0400

    created UI texts

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -20,61 +20,48 @@ public class FollowCam : MonoBehaviour {
 
     }
 
-    void FixedUpdate()
-    {
-        //if(POI==null){
-        //    return;
-        //}
+    void FixedUpdate() {
+        // if there's only one line following an if, it doesn't need braces
+        //if (POI == null) return; // return if there is no poi
+         
+         // Get the position of the poi
+         //Vector3 destination = POI.transform.position;
+         // Limit the X & Y to minimum values
+         destination.x = Mathf.Max (minXY.x, destination.x);
+         destination.y = Mathf.Max (minXY.y, destination.y);
+         // Interpolate from the current Camera position toward destination
+         destination = Vector3.Lerp (transform.position, destination, easing);
+         // Force destination.z to be camZ to keep the camera far enough away
+         destination.z = camZ;
+         // Set the camera to the destination
+         transform.position = destination;
+         // Set the orthographic of the Camera to keep Ground in view
+         Camera.main.orthographicSize = destination.y + 10;
 
+    }
 
-        ////Get POI position
-        //Vector3 destination = POI.transform.position;
 
         Vector3 destination;
-        if(POI==null){
+        // If there is no poi, return to P: (0,0,0)
+        if(POI==null) {
             destination = Vector3.zero;
 
-        }
-        else{
+        } else {
             //get the position of the poi
             destination = POI.transform.position;
-            //if poi is a profile check to see if it's at rest
+            //if poi is a projectile check to see if it's at rest
             if(POI.tag =="Projectile"){
                 //if is is sleeping(not moving)
-                if(POI.GetComponent<Rigidbody>().IsSleeping()){
+                if(POI.GetComponent<Rigidbody>().IsSleeping()) {
                     POI = null;
                     return;
                 }
             }
         }
 
-        //Limit X & Y to minimum values
-        destination.x = Mathf.Max(minXY.x, destination.x);
-        destination.y = Mathf.Max(minXY.y, destination.y);
-
-        //Interpolate from the current Camera position toward destination
-        destination = Vector3.Lerp(transform.position, destination, easing);
-
-        //force destination.z to be camZ to keep camera far enough away
-        destination.z = camZ;
-
-        //set the camera to the destination
-        transform.position = destination;
-
-        //Set the orthographicSize of the camera to keep ground in view
-        Camera.main.orthographicSize = destination.y + 10;
-
+        
 
 
     }
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
+   
--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Slingshot : MonoBehaviour {
     static private Slingshot S;
     // fields set in the Unity Inspector pane
+
     [Header("Set in Inspector")]
     public GameObject prefabProjectile;
     public float velocityMult = 8f;
@@ -15,8 +16,9 @@ public class Slingshot : MonoBehaviour {
     public Vector3 launchPos;
     public GameObject projectile;
     public bool aimingMode;
-
     private Rigidbody projectileRigidbody;
+
+    
 
     static public Vector3 LAUNCH_POS {
         get {
@@ -70,25 +72,27 @@ public class Slingshot : MonoBehaviour {
         Vector3 mousePos3D = Camera.main.ScreenToWorldPoint ( mousePos2D ) ;
 
         // Find the delta from the launchPos to the mousePos3D
-        Vector3 mouseDelta = mousePos3D-launchPos;
+        Vector3 mouseDelta = mousePos3D - launchPos;
         // Limit mouseDelta to the radius of the Slingshot SphereCollider
-        float maxMagnitude = this.GetComponent<SphereCollider> ().radius;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
         if (mouseDelta.magnitude > maxMagnitude) {
             mouseDelta.Normalize ();
             mouseDelta *= maxMagnitude;
         }
 
         // Move the projectile to this new position
-        Vector3 projPos = launchPos = mouseDelta;
+        Vector3 projPos = launchPos + mouseDelta;
         projectile.transform.position = projPos;
 
-        if ( Input.GetMouseButtonUp (0)) {
+        if ( Input.GetMouseButtonUp (0) ) {
             // The mouse has been released
             aimingMode = false;
             projectileRigidbody.isKinematic = false;
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
             FollowCam.POI = projectile;
             projectile = null;
+            MissionDemolition.ShotFired();
+            ProjectileLine.S.poi = projectile;
         }
     }
 
